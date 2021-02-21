1e864cb0359449914f59039879db7917c94e77e2
LJH <ljh2976@gmail.com>
2021-02-21 20:42:44 +0900

    11 로그인 기능 with Bcrypt

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # boiler-plate-ko
+노드 리액트 기초 강의 boiler-plate
 https://www.youtube.com/playlist?list=PL9a7QRYt5fqkZC9jc7jntD1WuAogjo_9T
 
 
@@ -13,3 +14,11 @@ Bcrypt를 이용하여 비밀번호를 암호화 후 DB에 저장
 https://www.npmjs.com/package/bcrypt
 
 npm install bcrypt --save
+
+## 11 로그인 기능 with Bcrypt
+
+1. 요청된 Email을 DB에서 찾기
+2. 요청된 Email이 DB에 있다면 PW확인
+3. PW가 맞다면 토큰 생성
+
+
--- index.js
+++ index.js
@@ -25,10 +25,34 @@ app.post('/register', (req, res) => {
 
     const user = new User(req.body)
 
-    user.save((err, doc) => {
+    user.save((err, userInfo) => {
         if(err) return res.json({ successl:false, err})
         return res.status(200).json({
             success:true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당되는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({loginSuccess:false, message: "비밀번호가 틀렸습니다."})
+            }
+
+            // 비밀번호가 맞다면 토큰을 생성
+            user.generateToken((err, user) => {
+
+            })
         })
     })
 })
