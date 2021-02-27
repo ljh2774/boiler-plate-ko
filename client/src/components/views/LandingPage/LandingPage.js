04c5c6202d59ccf6a41c3920a979156a419b0b30
LJH <ljh2976@gmail.com>
2021-02-28 02:16:36 +0900

    #32 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,25 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
-        .then(response => {
-            console.log(response)
-        })
+            .then(response => {
+                console.log(response)
+            })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success) {
+                    props.history.push("/login")
+                } else {
+                    alert('로그아웃하는데 실패했습니다.')
+                }
+            })
+    }
 
     return (
         <div style={{
@@ -17,6 +28,9 @@ function LandingPage() {
         }}>
             <h2>시작 페이지</h2>
 
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
             
         </div>
     )
